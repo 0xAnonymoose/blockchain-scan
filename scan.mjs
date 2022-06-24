7c4a2d0199c0468c52858b18e050158172f21f35
Anonymoose <anonymoose0x@proton.me>
2022-06-24 18:48:24 -0400

    added caching to scanner

--- scan.mjs
+++ scan.mjs
@@ -1,19 +1,40 @@
-import { applySignature, downloadContract, downloadTransaction } from './signature.mjs';
-import { readFileSync } from 'fs'
+import { applySignature, prepareContract, downloadTransaction } from './signature.mjs';
+import { readFileSync, writeFileSync } from 'fs'
 import { parse } from 'yaml'
 import Web3 from 'web3';
 
 const web3 = new Web3("https://bsc-dataseed1.binance.org");
 const argv = process.argv;
 
-async function prepareTransactions(transactions) {
+function _saveCache(fname, data) { 
+  writeFileSync(fname, data);
+}
+
+function _loadCache(fname) {
+  try {
+     let rawdata = readFileSync(fname, 'utf-8');
+     return rawdata;
+  } catch(err) {
+     return null;
+  }
+}
+
+async function cachedTransaction( txnHash ) {
+  let c = _loadCache('cache/'+txnHash+'.json');
+  if (c != null) { return JSON.parse(c); }
+  c = await downloadTransaction(txnHash);
+  _saveCache('cache/'+txnHash+'.json', JSON.stringify(c) );
+  return c;
+}
+
+async function loadTargetTransactions(transactions) {
   let targets = [];
   
   console.log('Fetching',transactions.length,'transactions');
   for (let txnHash of transactions) {
     console.log(txnHash);
     try {
-      targets.push( await downloadTransaction(txnHash) );
+      targets.push( await cachedTransaction(txnHash) );
     } catch(e) {
       console.error('ERROR:', e);
     }
@@ -22,14 +43,22 @@ async function prepareTransactions(transactions) {
   return targets;
 }
 
-async function prepareContracts (addrs) {
+async function cachedContract( addr ) {
+  let c = _loadCache('cache/'+addr+'.hex');
+  if (c != null) { return prepareContract(addr,c); }
+  c = await web3.eth.getCode(addr);
+  _saveCache('cache/'+addr+'.hex', c);
+  return prepareContract(addr,c);
+}
+
+async function loadTargetContracts (addrs) {
   let targets = [];
   
   console.log('Fetching',addrs.length,'contracts');
   for (let addr of addrs) {
     if (addr == '') { continue; }
     console.log(addr);    
-    targets.push( await downloadContract(addr) );
+    targets.push( await cachedContract(addr) );
   } 
     
   return targets;
@@ -59,7 +88,7 @@ if (argv.length < 5) {
       addrs = data.split('\n');
     }
 
-    targets = await prepareContracts(addrs);
+    targets = await loadTargetContracts(addrs);
 
   } else if (op === 'block' || op === 'transaction') {
     // build target transaction list
@@ -71,14 +100,14 @@ if (argv.length < 5) {
     } 
         
     // load transactions
-    targets = await prepareTransactions(transactions);
+    targets = await loadTargetTransactions(transactions);
 
   } else if (op == 'validate') {
     let deploys = parse(readFileSync( arg, 'utf-8' ));
     
     // load both contracts and transactions
-    targets = [...await prepareTransactions(Object.keys(deploys.transactions)),
-               ...await prepareContracts(Object.keys(deploys.contracts))];
+    targets = [...await loadTargetTransactions(Object.keys(deploys.transactions)),
+               ...await loadTargetContracts(Object.keys(deploys.contracts))];
      
     // annotate with expected results          
     for (let target of targets) {
@@ -99,20 +128,21 @@ if (argv.length < 5) {
   } else {
     let hits = 0;
     for (let target of targets) {
-        
+   
       let match = false;
       let res;
       for (let sig of malwares.signatures) {
+
         if (sig.signatureType == 'source') { continue; }
         res = applySignature( sig, target );
         if (res.match) {
-          console.error(target.contractAddress || target.transactionHash, ' MATCHED ',malwares.malwareName, malwares.malwareTags);       
-          console.dir(res);
+          console.error(target.contractAddress || target.transactionHash, ' MATCHED ',malwares.malwareName, malwares.malwareTags, JSON.stringify(res));       
           match = true;
           break;
         }
       }
 
+      // did we expected to match? (validate)
       if (target.hasOwnProperty('_expected') && match) {
         let ex = target._expected;
         for (let f of Object.keys(ex)) {
@@ -126,9 +156,9 @@ if (argv.length < 5) {
           
       if (!match) {
         if (target.hasOwnProperty('_expected')) {
-          console.error('FAIL   expected to match');
+          console.error('FAIL   expected to match ', target.transactionHash || target.contractAddress);
         } else {
-          console.log(target.contractAddress || target.transactionHash, 'clean');
+          console.log(target.transactionHash || target.contractAddress, 'clean');
         }
       } else {
         hits++;
